4bd2cab835488b8a28c9518cdd133f70cebf0ef2
MarkusThill <8896197+MarkusThill@users.noreply.github.com>
2025-12-30 14:21:29 +0000

    Deploying to gh-pages from @ MarkusThill/MarkusThill.github.io@60f33b240f153e3e41bead0db865a00cb9efd594 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -499,13 +499,6 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_bitbully_databases/";
             },},{
-        id: 'social-cv',
-        title: 'CV',
-        section: 'Socials',
-        handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
-        },
-      },{
         id: 'social-github',
         title: 'GitHub',
         section: 'Socials',
